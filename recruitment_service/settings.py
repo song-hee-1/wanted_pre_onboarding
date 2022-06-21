50b4fff0da4c62361833d71680e719da3e918bdc
한송희 <98_han@naver.com>
2022-06-21 13:15:12 +0900

    Feat : connect postgresql on azure

--- recruitment_service/settings.py
+++ recruitment_service/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'recruitment_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wanted_pre_onboarding',
+        'USER': os.environ["USER"],
+        'PASSWORD': os.environ["PASSWORD"],
+        'HOST': os.environ["HOST"],
+        'PORT': '5432',
     }
 }
 
